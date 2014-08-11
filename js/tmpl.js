c1f69f06e8b5661efd7b458a76e64b48a3f50a02
Doug Kumagai <omnidestroyer@gmail.com>
2014-08-10 21:51:01 -0600

    adding graceful fail on non-existent properties

--- js/tmpl.js
+++ js/tmpl.js
@@ -20,7 +20,7 @@ var Tmpl = (function(){
         for(var key in this.bindings){
             var element = queryElementInList(this.elements, key)
             if(element && getFirstLevelProp(this.bindings[key]) == change.name){
-              element.innerText = traverseObjectProps(change.object, this.bindings[key]);
+              element.innerText = traverseObjectProps(change.object, this.bindings[key]) || "";
             }
         }
     }
@@ -30,7 +30,11 @@ var Tmpl = (function(){
       var prop = obj;
       for(var i = 0; i < keys.length; i++){
         if(keys[i]){
-          prop = prop[keys[i]];
+			if(prop[keys[i]]){
+				prop = prop[keys[i]];
+			}else{
+				return null;
+			}
         }
       }
       return prop;
